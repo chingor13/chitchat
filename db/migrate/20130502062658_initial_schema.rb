d928ae4bd8b3c0ed5e53966527d5e63584ddca66
Jeff Ching <jching@avvo.com>
2013-05-01 23:33:38 -0700

    default schema

---
+++ db/migrate/20130502062658_initial_schema.rb
@@ -0,0 +1,26 @@
+class InitialSchema < ActiveRecord::Migration
+  def up
+    create_table :users do |t|
+      t.string :identifier
+      t.boolean :available
+      t.timestamps
+    end
+
+    create_table :calls do |t|
+      t.integer :from_id, :null => false
+      t.integer :to_id, :null => false
+      t.timestamps
+    end
+
+    create_table :messages do |t|
+      t.integer :call_id, :null => false
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :messages
+    drop_table :calls
+    drop_table :users
+  end
+end
