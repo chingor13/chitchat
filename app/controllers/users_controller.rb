42755ed36c19d59a6e11b156d92bb5a2a7f9defd
David Doan <david@avvo.com>
2013-05-02 08:01:46 -0700

    UsersController should return correct responses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,30 @@
 class UsersController < ApplicationController
 
-  before_filter :load_user, :only => [:sign_on, :sign_off, :show]
+  before_filter :load_user, only: [:sign_on, :sign_off, :show]
 
   # GET /users/:id
   def show
+    render json: {status: response.status, user: @user.to_hash}
   end
 
   # POST /users/:id/sign_on
   def sign_on
     @user.sign_on!
+    
+    render json: {status: response.status}
   end
 
   # POST /users/:id/sign_off
   def sign_off
     @user.sign_off!
+    
+    render json: {status: response.status}
   end
 
   protected
 
   def load_user
-    @user = Chitchat::User.find(params[:id])
+    @user = Chitchat::User.find_by_identifier!(params[:id])
   end
 
 end
--- app/models/chitchat/user.rb
+++ app/models/chitchat/user.rb
@@ -1,15 +1,20 @@
 module Chitchat
   class User < ActiveRecord::Base
-    has_many :messages, :inverse_of => :user
+    has_many :messages, inverse_of: :user
 
-    validates :identifier, :presence => true, :uniqueness => true
+    validates :identifier, presence: true, uniqueness: true
 
     def sign_on!
-      update_attributes!(:available => true)
+      update_attributes!(available: true)
     end
 
     def sign_off!
-      update_attributes!(:available => false)
+      update_attributes!(available: false)
     end
+    
+    def to_hash
+      {identifer: identifier, available: available}
+    end
+    
   end
 end
