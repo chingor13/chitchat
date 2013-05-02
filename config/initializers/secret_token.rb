af5e5c60f590a89c015e968094c6efcdde5bd993
Jeff Ching <jching@avvo.com>
2013-05-01 23:02:11 -0700

    add rails 4 skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chitchat::Application.config.secret_key_base = '78aaf65dbc497278ecb77a49637d3c9c33be9943baf1f307ccbd602e3507cc9081c622d43666938636bcbbab8a12e74d7f75fa40e76c565ba369212ad4f13c17'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chitchat::Application.config.session_store :cookie_store, key: '_chitchat_session'
